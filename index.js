62821f867419d12205c50e421b6fd96fa0ea5fdd
Adryel Couto <adryelcouto@Laptop-de-Adryel.local>
2024-03-01 03:07:45 -0300

    arrumando header

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 40)
+});
 function mobileMenu(){
 
     let hamburger = document.querySelector(".navigation")
